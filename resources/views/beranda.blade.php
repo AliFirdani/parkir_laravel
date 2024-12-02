b3a2039a1231dbfcab2ec5da6e474df711151351
AliFirdani <alifirdani2779@gmail.com>
2024-12-03 02:43:21 +0700

    logo

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -10,7 +10,12 @@
 
       <nav class="navbar navbar-expand-lg bg-warning border-bottom border-body-tertiary">
           <div class="container-fluid">
-              <a class="navbar-brand" href="#">PARK.ing</a>
+
+                <a class="navbar-brand" href="#">
+                  <img src="images/image.png" alt="" width="30" height="30">
+                </a>
+
+              <a class="navbar-brand" href="#">PARK.ing Mobile</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -23,43 +28,17 @@
           <a class="nav-link active" href="halaman">LOGIN</a>
           </li>
       </ul>
-      <
     </div>
   </div>
 </nav>
+<div class="container text-center">
+    <div class="row">
+        <div class="col">
 
-   <div id="carouselExampleFade" class="carousel slide carousel-fade position-absolute top-0 start-50 translate-middle">
-         <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="images/download.jpg" class="d-block w-50" alt="50">
-          </div>
-          <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="...">
-          </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
-<div class="position-absolute top-49 start-50 translate-middle-x mt-5">
-    <div class="card" style="width: 50rem;">
-  <div class="p-4 mb-3 bg-warning text-dark">
-    <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-dark">Go somewhere</a>
     </div>
-   </div>
- </div>
+    <img src="images/parkiran.jpg" class="d-block w-50" alt="">
 </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
--- resources/views/halaman.blade.php
+++ resources/views/halaman.blade.php
@@ -6,11 +6,11 @@
     <title>parkir</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
-  <body>
+  <body class="bg-secondary-subtle">
 
     <nav class="navbar navbar-expand-lg bg-warning border-bottom border-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">PARK.ing</a>
+    <a class="navbar-brand" href="#">PARK.ing Mobile</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -23,7 +23,6 @@
           <a class="nav-link active" href="#">LOGIN</a>
         </li>
       </ul>
-
     </div>
   </div>
 </nav>
